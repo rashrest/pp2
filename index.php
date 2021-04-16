87f3d55fc9208d4039db9814fb674c5536195a77
Raju Shrestha <rshrestha9@mail.greenriver.edu>
2021-04-16 16:14:05 -0400

    Added document body and block in PHP

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+//Name: Raju Shrestha
+//04/16/2021
+//http://rshrestha9.greenriverdev.com/sdev328/pp2/
+//Turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program2</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+echo "<h1>PHP Array Practice."
+?>
